5d68538859c39b0479fa447d3281e5a5aa8851fe
Damian Zaremba <damian@damianzaremba.co.uk>
2011-03-15 22:20:42 +0000

    Updating README to say something and adding in a quick script to rebuild the build env

---
+++ setupenv.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+#############################################################
+# Simple bash script to rebuild my rpm build environment    #
+# Might be of use to other people.                          #
+#                                                           #
+# Expects a clean env and pulls down data from github.      #
+#############################################################
+
+CENTOS_REPO="https://github.com/DamianZaremba/rpm-centos.git";
+BASE_DIR="/home/rpmbuild"
+
+function initial_check {
+	cd $BASE_DIR
+	echo "--> We are going to install into $(pwd)"
+
+	echo "Is this ok? [y/n]"
+	read rep
+
+	if [ "$rep" == "y" ];
+	then
+		return
+	else
+		echo "Exiting";
+		exit 1;
+	fi
+}
+
+function setup_rpm_stuff {
+	echo "Setting up rpm env"
+	cat > "$BASE_DIR/.rpmmacros" <<EOF
+%_topdir $BASE_DIR
+%packager Damian Zaremba <damian@damianzaremba.co.uk>
+EOF
+
+	test -d "$BASE_DIR/rpmbuild" || mkdir "$BASE_DIR/rpmbuild/"
+	test -d "$BASE_DIR/rpmbuild/BUILD" || mkdir "$BASE_DIR/rpmbuild/BUILD/"
+	test -d "$BASE_DIR/rpmbuild/RPMS" || mkdir "$BASE_DIR/rpmbuild/RPMS/"
+	test -d "$BASE_DIR/rpmbuild/SOURCES" || mkdir "$BASE_DIR/rpmbuild/SOURCES/"
+	test -d "$BASE_DIR/rpmbuild/SPECS" || mkdir "$BASE_DIR/rpmbuild/SPECS/"
+	test -d "$BASE_DIR/rpmbuild/SRPMS" || mkdir "$BASE_DIR/rpmbuild/SRPMS/"
+}
+
+function setup_centos {
+	setup_rpm_stuff
+
+	if [ -d "$BASE_DIR/rpm-centos" ];
+	then
+		cd "$BASE_DIR/rpm-centos"
+		git pull
+	else
+		cd $BASE_DIR
+		git clone $CENTOS_REPO
+		cd "$BASE_DIR/rpm-centos"
+	fi
+
+	cd "$BASE_DIR/rpm-centos"
+	for pkg in *;
+	do
+		test -d "$BASE_DIR/rpm-centos/$pkg" || continue;
+		cd "$BASE_DIR/rpm-centos/$pkg";
+		echo "Starting $pkg"
+
+		for arch in *;
+		do
+			test -d "$BASE_DIR/rpm-centos/$pkg/$arch" || continue;
+			test -d "$BASE_DIR/rpmbuild/RPMS/$arch/" || mkdir "$BASE_DIR/rpmbuild/RPMS/$arch/";
+			cd "$BASE_DIR/rpm-centos/$pkg/$arch"
+			echo ".. Processing $arch"
+
+			echo ".... Copying specs for $pkg ($arch)"
+			find "$BASE_DIR/rpm-centos/$pkg/$arch" -type f -iname *.spec -exec cp {} $BASE_DIR/rpmbuild/SPECS/ \;
+
+			echo ".... Copying rpms for $pkg ($arch)"
+			find "$BASE_DIR/rpm-centos/$pkg/$arch" -type f -iname *.rpm -exec cp {} $BASE_DIR/rpmbuild/RPMS/$arch/ \;
+
+			echo ".... Copying srpms for $pkg ($arch)"
+			find "$BASE_DIR/rpm-centos/$pkg/$arch" -type f -iname *.srpm -exec cp {} $BASE_DIR/rpmbuild/SRPMS/ \;
+
+			echo ".... Copying sources for $pkg ($arch)"
+			find "$BASE_DIR/rpm-centos/$pkg/$arch" -type f ! -iname *.spec ! -iname *.srpm ! -iname *.rpm -exec cp {} $BASE_DIR/rpmbuild/SOURCES/ \;
+
+			echo ""
+		done
+	done
+}
+
+if [ -z "$1" ] || [ "$1" == "--help" ];
+then
+	echo "##################################################"
+	echo "# RPM Build Env Builder                          #"
+	echo "#                                                #"
+	echo "# Usage: buildenv.sh [--help | --setup-centos]   #"
+	echo "#                                                #"
+	echo "# --help                                         #"
+	echo "#     Prints out this message                    #"
+	echo "#                                                #"
+	echo "# --setup-centos                                 #"
+	echo "#     Sets up a centos build env                 #"
+	echo "#                                                #"
+	echo "# ---------------------------------------------- #"
+	echo "# WARNING                                        #"
+	echo "#     This will over write stuff                 #"
+	echo "##################################################"
+
+else if [ "$1" == "--setup-centos" ];
+then
+	initial_check
+
+	echo "Starting setup";	
+	setup_centos
+
+else
+	echo "I have no idea what you want me to do";
+	exit 1;
+fi
+fi
